8020ad6524e875bcc7ca22971b2caeba69b2aa19
Turlybek Zhuranov <turlybekk@gmail.com>
2020-10-18 12:35:52 +0600

    feat: implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let temp;
+  while (str) {
+    for (bracket of bracketsConfig) {
+      str = str.replace(bracket.join(''), '')
+    }
+    if (str === temp) {
+      return false;
+    }
+    temp = str;
+  }
+  return str.length === 0;
 }
